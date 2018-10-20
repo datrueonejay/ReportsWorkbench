272b24a438d55227858eb9801632eec8efbbb625
Jayden Arquelada <jayarquelada@gmail.com>
2018-10-20 16:02:00 -0400

    Implemented UI and file selection for data upload screen

---
+++ app/src/main/java/com/tminions/app/DataUploadScreenController.java
@@ -0,0 +1,102 @@
+package com.tminions.app;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.scene.control.ListView;
+import org.tbee.javafx.scene.layout.fxml.MigPane;
+
+import java.io.File;
+import java.util.List;
+
+public class DataUploadScreenController {
+
+    private ObservableList filesToUpload = FXCollections.observableArrayList();
+    private String templateType;
+
+    @FXML private GridPane root;
+    @FXML private ListView<File> filesToUploadUi;
+    @FXML private Button uploadButton;
+    @FXML private ComboBox<String> templateTypesUi;
+
+    public void initialize() {
+        // Get templates from wherever they are stored
+        templateTypesUi.setItems(getTemplates());
+        // Set up listener for when a template type is selected
+        templateTypesUi.valueProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue value, String oldSelection, String newSelection) {
+                templateType = newSelection;
+                updateUploadButton();
+            }
+        });
+    }
+
+    public void selectFiles() {
+        // Set up file chooser
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Add iCare Files To Submit");
+        fileChooser.getExtensionFilters().addAll(
+                new ExtensionFilter("Excel Files (97 - 2003) .xls", "*.xls"),
+                new ExtensionFilter("Excel Files .xlsx", "*.xlsx "),
+                new ExtensionFilter("All Files", "*.*"));
+        // Let user pick files
+        List<File> selectedFiles = fileChooser.showOpenMultipleDialog(root.getScene().getWindow());
+        // Loop through files if at least one file is selected
+        if (selectedFiles != null) {
+            // Loop through all files selected
+            for (File currFile : selectedFiles) {
+                if (!filesToUpload.contains(currFile)) {
+                    addFile(currFile);
+                }
+            }
+        }
+    }
+
+    public void uploadFiles() {
+        //TODO: Parse files and upload to server
+    }
+
+    public void removeSelectedFile() {
+        // Get index of selected file
+        int index = filesToUploadUi.getSelectionModel().getSelectedIndex();
+        if (index != -1) {
+            // Remove selected file
+            filesToUpload.remove(index);
+            // Update list and ui
+            filesToUploadUi.setItems(filesToUpload);
+            updateUploadButton();
+        }
+    }
+
+    private void addFile(File file) {
+        filesToUpload.add(file);
+        filesToUploadUi.setItems(filesToUpload);
+        updateUploadButton();
+    }
+
+    private ObservableList<String> getTemplates() {
+        // TODO: Replace with actual code to get templates frm wherever they are stored
+        return FXCollections.observableArrayList(
+                "Employment Services",
+                "Language Services",
+                "Other Services"
+        );
+    }
+
+    private void updateUploadButton() {
+        if (filesToUpload.size() > 0 && templateType != null) {
+            uploadButton.setDisable(false);
+        } else {
+            uploadButton.setDisable(true);
+        }
+    }
+
+}
