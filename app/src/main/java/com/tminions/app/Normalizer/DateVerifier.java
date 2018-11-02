42a915a35941f282f634b77fb32f3366a936e8a6
Balaji Babu <Bbabuhub@github.com>
2018-11-02 17:07:35 -0400

    finished postalcode verifier and phoneverifier need to twear dateverifier to handle more cases

--- app/src/main/java/com/tminions/app/Normalizer/DateVerifier.java
+++ app/src/main/java/com/tminions/app/Normalizer/DateVerifier.java
@@ -1,13 +1,22 @@
 package com.tminions.app.Normalizer;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
 
 public class DateVerifier implements Verifier{
 
     static final String columnHeaderRegex = "(YYYY-MM-DD)";
-
+    
     @Override
-    public String verify(String cellString) {
+    public String verify(String cellString) throws InvalidFormatException {
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    	try {
+    	    LocalDate date = formatter.parse(cellString, LocalDate::from);
+    	} catch (DateTimeParseException e) {
+    	    throw new InvalidFormatException();
+    	}
         return cellString;
-        //TODO: @Balaji implement
     }
 
     @Override
