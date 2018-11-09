5a4f061beb1f03dc1202bdabb4487622e166e8db
Balaji Babu <Bbabuhub@github.com>
2018-11-08 23:21:30 -0500

    finished UI for report generation tab

---
+++ app/src/main/java/com/tminions/app/ReportGenerationScreen.java
@@ -0,0 +1,26 @@
+package com.tminions.app;
+
+import com.tminions.app.controllers.OrgUploadTimeController;
+import com.tminions.app.models.OrgUploadTimeModel;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ReportGenerationScreen {
+			
+
+    public void initialize() {
+       
+    }
+    
+    public void selectReport() {
+    	
+    }
+    
+    public void selectReport2() {
+    	
+    }
+
+}
